10b70778af72cb21ed4146f30cf73bc0d2a4ee55
mjarsater <martin.jarsater@hotmail.com>
2020-10-28 13:18:51 +0100

    Uppgift 3.2.7 - Hjälpprogram?

--- Uppgift 3.2.7/index.js
+++ Uppgift 3.2.7/index.js
@@ -2,3 +2,10 @@ $(function () {
     $("#box").draggable();
     $("#date").datepicker();
 });
+
+$(function () {
+    $("#button").on("click", function () {
+        $(".firstClass").switchClass("firstClass", "otherClass", 500);
+        $(".otherClass").switchClass("otherClass", "firstClass", 500);
+    });
+});
