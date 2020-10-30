0cc035a5676949aa5143d523677ba6915277c34f
mjarsater <martin.jarsater@hotmail.com>
2020-10-30 14:57:46 +0100

    Uppgift 3.4.4

---
+++ Uppgift 3.4.4/index.js
@@ -0,0 +1,27 @@
+function logResults(json) {
+    console.log(json.data);
+
+    document.getElementById("username").append(" " + json.data.login);
+    document.getElementById("repos").append(" " + json.data.public_repos);
+    document.getElementById("followers").append(" " + json.data.followers);
+    document.getElementById("avatar").src = json.data.avatar_url;
+    document.getElementById("created_at").append(" " + json.data.created_at);
+    document.getElementById("updated_at").append(" " + json.data.updated_at);
+
+
+    console.log(json.data.avatar_url);
+}
+
+$.ajax({
+    url: `https://api.github.com/users/mjarsater`,
+    dataType: "jsonp",
+    jsonpCallback: "logResults"
+});
+
+
+
+
+
+
+
+
