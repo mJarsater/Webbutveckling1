7f54499bc82dcd0876c08f8c75481507d6d68f87
mjarsater <martin.jarsater@hotmail.com>
2020-10-30 09:23:36 +0100

    Uppgift 3.1.2

--- Uppgift 3.1.2/index.js
+++ Uppgift 3.1.2/index.js
@@ -1,32 +1,22 @@
-$(document).ready(function () {
+var myVariable;
+
+window.onload = function () {
+    myVariable = setInterval(timer, 1000);
     alert("Den här hemsidan behövs för denna uppgift");
     confirm("Har du förstått?");
     languageFunction();
+
     var favoriteLanguage = prompt("Vad är ditt favoritspråk att programmera i?");
     if (favoriteLanguage != "") {
         document.getElementById("greeting").textContent = "Vad roligt att höra, jag gillar också " + favoriteLanguage + "!";
     } else {
         document.getElementById("greeting").textContent = "Det är okej. Alla kan inte ha en favorit.";
     }
-
-    var myVariable = setInterval(timer, 1000);
-
     function timer() {
         var date = new Date();
         var time = date.toLocaleTimeString();
         document.getElementById("time").textContent = time;
     }
-
-    var myWindow;
-
-    function openFuntion() {
-        myWindow = window.open("https://www.w3schools.com", "_blank", "width=500, height=500");
-    }
-
-    function closeFunction() {
-        myWindow.close();
-    }
-
 
     setInterval(function () {
         alert("Är du kvar?");
@@ -37,21 +27,30 @@ $(document).ready(function () {
         alert("Nu har du varit här länge...");
     }, 5000);
 
-
-
-    function stopTime() {
-        clearInterval(myVariable);
-    }
-
-    function noTimeout() {
-        clearTimeout(myOtherVar)
-    }
-
     function languageFunction() {
         var text = "Jag ser att du använder " +
             navigator.language
             + " för din browser";
         document.getElementById("language").innerHTML = text;
+
     }
 
-});
+}
+
+var myWindow;
+
+function openFuntion() {
+    myWindow = window.open("https://www.w3schools.com", "_blank", "width=500, height=500");
+}
+
+function closeFunction() {
+    myWindow.close();
+}
+
+function stopTime() {
+    clearInterval(myVariable);
+}
+
+function noTimeout() {
+    clearTimeout(myOtherVar)
+}
