43cc281fb5b03c6f04596331013dacf960d2a230
mJarsater <martin.jarsater@hotmail.com>
2020-10-30 10:14:17 +0100

    Uppgift 3.3.3

--- Uppgift 3.3.3/index.js
+++ Uppgift 3.3.3/index.js
@@ -1,27 +1,43 @@
-function getDatabase() {
-    var dbName = document.getElementById("dbName").value;
-    var dataBase = document.getElementById("database");
-    window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
-    var db;
+window.onload = function () {
 
-    if (!window.indexedDB) {
-        console.log("Your browser doesn't support a stable version of IndexedDB. Such and such feature will not be available.");
+    window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+
+
+    if (!window.indexedDB) { alert("Error"); }
+    var openBtn = document.getElementById("openBtn");
+
+
+    openBtn.onclick = function () {
+        var dbName = document.getElementById("dbName").value;
+
+        console.log(dbName);
+        var request = window.indexedDB.open(dbName, 1);
+
+        request.onupgradeneeded = function (event) {
+            var db = event.target.result;
+            var store = db.createObjectStore(dbName + "Store", { autoIncrement: true });
+            store.add("Test1");
+            store.add("Test2");
+
+            console.log(db + " - Upgrade");
+
+
+        }
+        request.onsuccess = function (event) {
+            var db = event.target.result;
+            console.log(db + " - Success");
+            var transaction = db.transaction(dbName + "Store", "readwrite");
+            var store = transaction.objectStore(dbName + "Store");
+
+            var one = store.get(1);
+            var two = store.get(2);
+        }
+
     }
 
-    var request = window.indexedDB.open(dbName);
-
-    request.onerror = function (event) {
-        console.log("Error");
-    };
-    request.onupgradeneeded = function (event) {
-        console.log("Success!")
-        db = event.target.result;
-
-        var objectStore = db.createObjectStore(dbName, { keyPath: "tastTitle" });
 
 
-        dataBase.innerHTML += "<li>Object store created.</li>";
-    };
 
 
-}
+
+};
