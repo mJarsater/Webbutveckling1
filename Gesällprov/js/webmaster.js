a830a04172521e57279f9774f4a979245592094d
mJarsater <Martin.Jarsater@hotmail.com>
2020-11-24 15:59:53 +0100

    Gesällprov

--- Gesällprov/js/webmaster.js
+++ Gesällprov/js/webmaster.js
@@ -1,11 +1,8 @@
 function logResults(json) {
     console.log(json.data);
 
-    document.getElementById("username").append(" " + json.data.login);
-    document.getElementById("repos").append(" " + json.data.public_repos);
-    document.getElementById("followers").append(" " + json.data.followers);
+    document.getElementById("username").append(" " + json.data.login + "@GitHub");
     document.getElementById("avatar").src = json.data.avatar_url;
-    document.getElementById("created_at").append(" " + json.data.created_at);
     document.getElementById("updated_at").append(" " + json.data.updated_at);
 }
 
