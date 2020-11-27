9a368c7208cab6ad9cea2451c8af5013221cc966
mJarsater <Martin.Jarsater@hotmail.com>
2020-11-27 11:15:09 +0100

    Uppgift 3.2.4

--- Uppgift 3.2.4/index.js
+++ Uppgift 3.2.4/index.js
@@ -1,16 +1,18 @@
-function openTab(event, tabNumber) {
-    var i, tabContent, tabLink;
 
+// Funktion som öppnar en tabb
+function openTab(tabNumber) {
+    var i, tabContent;
+
+    /*  Hämtar alla element med klassen "tabContent" 
+        till arrayen tabContent*/
     tabContent = document.getElementsByClassName("tabContent");
+    // Loopar över alla element och sätter display none
     for (i = 0; i < tabContent.length; i++) {
         tabContent[i].style.display = "none";
     }
 
-    tabLink = document.getElementsByClassName("tabs");
-    for (i = 0; i < tabLink.length; i++) {
-        tabLink[i].className = tabLink[i].className.replace(" active", "");
-    }
-
+    // Hämtar elementet med "tabNumber" och lägger till display block.
     document.getElementById(tabNumber).style.display = "block";
-    event.currentTarget.className += " active";
+
+
 }
