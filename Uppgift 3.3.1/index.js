12082f0f5aa6c400066133fb920957f6a891d6b7
mJarsater <Martin.Jarsater@hotmail.com>
2020-12-01 16:20:13 +0100

    Uppgift 3.3.1

--- Uppgift 3.3.1/index.js
+++ Uppgift 3.3.1/index.js
@@ -1,24 +1,45 @@
 
 window.onload = function () {
+    // Hämtar alla element efter ID
     const inputKey = document.getElementById("key");
     const inputValue = document.getElementById("value");
-    const btn = document.getElementById("btn");
+    const sendBtn = document.getElementById("sendBtn");
     const localStorageOutput = document.getElementById("localStorageOutput");
+    const clearBtn = document.getElementById("clearBtn");
 
 
-
-
-    btn.onclick = function () {
+    // Funktion som skapar ett item för localstorage
+    sendBtn.onclick = function () {
+        // key får värdet av input i "key"
         const key = inputKey.value;
+        // value får värdet av input i "value"
         const value = inputValue.value;
 
-
+        // Kollar så att båda har ett värde
         if (key && value) {
+            // Skapar ett item och lägget till i localstorage
             localStorage.setItem(key, value);
+            // Laddar om sidan
             location.reload();
         }
     };
 
+    // Funktion som raderar allt localstorage
+    clearBtn.onclick = function () {
+        const localStorageOutput = document.getElementById("localStorageOutput");
+        // Clearar localstorage
+        localStorage.clear();
+
+        // För att visa att det faktiskt är borta.
+        const key = localStorage.key(1);
+        const value = localStorage.getItem(key);
+        // Skriver ut localstorage (som nu ska vara tomt.)
+        localStorageOutput.innerHTML = `Local Storage Cleared <br >Key: ${key}  Value: ${value}<br >`;
+
+
+    }
+
+    // Loopar igenom och skriver ut localstorage
     for (var i = 0; i < localStorage.length; i++) {
         const key = localStorage.key(i);
         const value = localStorage.getItem(key);
@@ -27,14 +48,3 @@ window.onload = function () {
     }
 
 };
-function clearLocalStorage() {
-    const localStorageOutput = document.getElementById("localStorageOutput");
-    localStorage.clear();
-
-    const key = localStorage.key(1);
-    const value = localStorage.getItem(key);
-
-    localStorageOutput.innerHTML = `Local Storage Cleared <br >Key: ${key}  Value: ${value}<br >`;
-
-
-}
