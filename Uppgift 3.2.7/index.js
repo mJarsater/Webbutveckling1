3c8d5d444b91b16c0e9da5ad5485f9c1fb506a5c
mJarsater <Martin.Jarsater@hotmail.com>
2020-12-01 10:44:09 +0100

    3.2.7

--- Uppgift 3.2.7/index.js
+++ Uppgift 3.2.7/index.js
@@ -1,11 +1,17 @@
+
 $(function () {
+    // Gör elementet med id "box" draggable
     $("#box").draggable();
+    // Skapar en datepicker av elementet med id "date" 
     $("#date").datepicker();
 });
 
 $(function () {
+    // Skapar en eventhandler för klick
     $("#button").on("click", function () {
+        // Byter mellan firstClass och otherClass på 0.5s
         $(".firstClass").switchClass("firstClass", "otherClass", 500);
+        // Byter mellan firstClass och otherClass på 0.5s
         $(".otherClass").switchClass("otherClass", "firstClass", 500);
     });
 });
