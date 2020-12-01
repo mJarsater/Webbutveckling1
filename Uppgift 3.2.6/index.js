585c09b86255b619b0a01582c4ae68589765676a
mJarsater <Martin.Jarsater@hotmail.com>
2020-12-01 10:39:21 +0100

    Uppgift 3.2.6

--- Uppgift 3.2.6/index.js
+++ Uppgift 3.2.6/index.js
@@ -1,36 +1,65 @@
 window.onload = function () {
+    // Sätter fokuset på inputfältet för namn
     document.getElementById("name").focus();
+    // Gömmer alla tre elementen
     $("#dataSystSection").hide();
     $("#erpSection").hide();
     $("#markItSection").hide();
+
+
+    // Hämtar och lägger till en eventlistner för alla radiobutton elementen.
+    var datasystRadioBtn = document.getElementById("datasystRadio");
+    datasystRadioBtn.addEventListener("click", dataSystFunction);
+    var erpRadioBtn = document.getElementById("erpRadio");
+    erpRadioBtn.addEventListener("click", erpFunction);
+    var markItRadioBtn = document.getElementById("markItRadio");
+    markItRadioBtn.addEventListener("click", markItFunction);
 }
 
-
+//Funktion som visar formuläret för Data- och Systemvetenskap, och gömmer de andra.
 function dataSystFunction() {
-    document.getElementById("dynamicForm").reset();
-    document.getElementById("datasystRadio").checked = true;
-    $("#dataSystSection").show();
-    $("#erpSection").hide();
-    $("#markItSection").hide();
+    var datasystRadio = document.getElementById("datasystRadio");
+    if (datasystRadio.checked) {
+        console.log("test")
+        // Resetar formuläret
+        document.getElementById("dynamicForm").reset();
+        // Kollar vilken radiobutton som är ikryssad
+        document.getElementById("datasystRadio").checked = true;
+        $("#dataSystSection").show();
+        $("#erpSection").hide();
+        $("#markItSection").hide();
+    }
 }
 
+//Funktion som visar formuläret för ERP, och gömmer de andra.
 function erpFunction() {
-    document.getElementById("dynamicForm").reset();
-    document.getElementById("erpRadio").checked = true;
-    $("#erpSection").show();
-    $("#dataSystSection").hide();
-    $("#markItSection").hide();
+    var erpRadio = document.getElementById("erpRadio");
+    if (erpRadio.checked) {
+        // Resetar formuläret
+        document.getElementById("dynamicForm").reset();
+        // Kollar vilken radiobutton som är ikryssad
+        document.getElementById("erpRadio").checked = true;
+        $("#erpSection").show();
+        $("#dataSystSection").hide();
+        $("#markItSection").hide();
+    }
 }
 
+//Funktion som visar formuläret för Mark It, och gömmer de andra.
 function markItFunction() {
-    document.getElementById("dynamicForm").reset();
-    document.getElementById("markItRadio").checked = true;
-    $("#markItSection").show();
-    $("#dataSystSection").hide();
-    $("#erpSection").hide();
-
+    var markItRadio = document.getElementById("markItRadio");
+    if (markItRadio.checked) {
+        // Resetar formuläret
+        document.getElementById("dynamicForm").reset();
+        // Kollar vilken radiobutton som är ikryssad
+        document.getElementById("markItRadio").checked = true;
+        $("#markItSection").show();
+        $("#dataSystSection").hide();
+        $("#erpSection").hide();
+    }
 }
 
+// Funktion som validerar att användaren har skrivit in de obligatoriska fälten
 function validateMarkIt() {
     var comment = document.forms["dynamicForm"]["commentMarkIt"].value;
     if (comment == "") {
@@ -40,6 +69,7 @@ function validateMarkIt() {
     return (true);
 }
 
+// Funktion som validerar att användaren har skrivit in de obligatoriska fälten
 function validateDataSyst() {
     var checkbox = document.querySelector('input[name="checkbox"]:checked');
     if (!checkbox) {
@@ -48,6 +78,7 @@ function validateDataSyst() {
     }
 }
 
+// Funktion som validerar att användaren har skrivit in de obligatoriska fälten
 function validateErp() {
     var favorite = document.forms["dynamicForm"]["favoriteErp"].value;
     if (favorite == "") {
@@ -56,6 +87,8 @@ function validateErp() {
     }
 }
 
+
+// Switchsats som triggar "rätt" validate funktion
 function validate() {
     var radioValue = $("input[name='radio']:checked").val();
     switch (radioValue) {
