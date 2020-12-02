7a6ee04b140ff09732087295a370e81c257d7878
mJarsater <Martin.Jarsater@hotmail.com>
2020-12-02 15:57:00 +0100

    Uppgift 3.3.1

--- Uppgift 3.3.1/popUp.js
+++ Uppgift 3.3.1/popUp.js
@@ -1,16 +1,23 @@
 window.onload = function () {
+    // Hämtar elementet med id "sessionStorage"
     var sessionStorageOutput = document.getElementById('sessionStorage');
+    // Hämtar elemetet med som "heter" item i sessionStorage
     var sessionMsg = sessionStorage.getItem('comment');
-
+    // Skriver ut elemetet
     sessionStorageOutput.innerHTML += `${sessionMsg}<br >`;
+
+    // Skapar en eventlistner för klick som kallar på clearSession()
+    document.getElementById("clearBtn").addEventListener("click", clearSession);
 
 }
 
+
+// Funktion som tömmer sessionStorage
 function clearSession() {
     sessionStorage.clear();
-    console.log(sessionStorage.getItem('comment'));
+
     var sessionStorageOutput = document.getElementById('sessionStorage');
     var sessionMsg = sessionStorage.getItem('comment');
-
+    // Skriver ut sessionStorage för att visa att det faktiskt är tomt
     sessionStorageOutput.innerHTML = `Msg cleared: ${sessionMsg}<br >`;
 }
--- Uppgift 3.3.1/sessionStorage.js
+++ Uppgift 3.3.1/sessionStorage.js
@@ -1,17 +1,24 @@
 window.onload = function () {
+
+    // Hämtar elementen med respektive ID
     const inputValue = document.getElementById("value");
     const btn = document.getElementById("btn");
 
-
+    // onclick funktion för btn
     btn.onclick = function () {
+        // Rensar sessionStorage
         sessionStorage.clear();
+        // lägger värdet i value som value för sessionStorage
         const value = inputValue.value;
+        // Skapar ett sessionStorageobjekt med "namn" comment och värdet i value
         sessionStorage.setItem('comment', value);
-        openFuntion();
-        console.log(sessionStorage.getItem('comment'));
+        // Kallar på openFunction
+        openFunction();
+
     };
 };
 
-function openFuntion() {
+// Funktion som öppnar ett nytt fönster, där sessionStorage visas
+function openFunction() {
     myWindow = window.open("http://127.0.0.1:5500/Uppgift%203.3.1/popUp.html", "_blank", "width=500, height=500");
 }
