8b1c426e3d965f90c0e7600ab7831ef38330b6e7
mJarsater <Martin.Jarsater@hotmail.com>
2020-12-03 10:25:23 +0100

    Uppgift 3.3.2

--- Uppgift 3.3.2/js/dropFile.js
+++ Uppgift 3.3.2/js/dropFile.js
@@ -1,21 +1,38 @@
+/*  Lägger en eventlistner på hela dokumentet
+    som stoppar defaultfunktion för dragover
+        */
 document.addEventListener('dragover', function (e) {
     e.preventDefault();
     e.stopPropagation();
 
 });
+
+/*  Lägger en eventlistner på hela dokumentet
+    som stoppar defaultfunktion för drop
+        */
 document.addEventListener('drop', function (e) {
     e.preventDefault();
     e.stopPropagation();
+    // Kallar istället på funktionen handleFiles
     handleFile(e.dataTransfer.files);
 });
 
+
+/*  Funktion som skapar en filereader och
+    visar filens innhåll på sidan.     */
 function handleFile(files) {
+    // Skapar ny filereader
     const reader = new FileReader();
+    // Hämtar elementet av "typen" img
     const preview = document.querySelector('img');
+    /*  Lägger en evenlistner på reader
+        som förhandsvisar bilder i webbläsaren */
     reader.addEventListener("load", function () {
         preview.src = reader.result;
     }, false);
 
+    // Läser in filens data
+    // reader.result filens data
     if (files) {
         reader.readAsDataURL(files[0]);
     }
--- Uppgift 3.3.2/js/index.js
+++ Uppgift 3.3.2/js/index.js
@@ -1,14 +1,23 @@
 window.onload = function () {
+
+    // Hämtar elementen med respektiva ID
     var input = document.getElementById('fileInput');
     var output = document.getElementById('output');
 
+    // Lägger en eventlistner på input
     input.addEventListener('change', function (event) {
-        console.log(input.files);
+
+        // Skapar en nya filereader
         const reader = new FileReader();
+
+        // Läser in filen som text
+        reader.readAsText(input.files[0]);
+
+        /*  När filen har lästs in så skrivs
+            dess innehåll ut på webbsidan*/
         reader.onload = function () {
             output.append(reader.result);
         }
-        reader.readAsText(input.files[0]);
     });
 
 }
