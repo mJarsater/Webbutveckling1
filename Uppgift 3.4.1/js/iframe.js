7c341279b341b5d6fcc4004195c6cc1044a778a0
mJarsater <Martin.Jarsater@hotmail.com>
2020-12-03 12:56:46 +0100

    3.4.1

--- Uppgift 3.4.1/js/iframe.js
+++ Uppgift 3.4.1/js/iframe.js
@@ -1,11 +1,16 @@
+
+// Lägger en eventlistner på hela fönstret
 window.addEventListener('message', receiver, false);
+
+// Funktion som hämtar ett meddelande
 function receiver(e) {
+    // Hämtar elementet med ID "message"
     var message = document.getElementById("message");
+    // Kollar så att meddelandet kommer ifrån "rätt" adress
     if (e.origin != 'http://127.0.0.1:5500/Uppgift%203.4.1/html/index.html') {
-        if (e.data == "Meddelande") {
-            message.innerHTML += `${e.data}`
-        } else {
-            alert(e.data);
-        }
+        message.innerHTML += `${e.data}`
+    } else {
+        // Annars -> Alert
+        alert(e.data);
     }
 }
--- Uppgift 3.4.1/js/index.js
+++ Uppgift 3.4.1/js/index.js
@@ -1,11 +1,19 @@
 window.onload = function () {
 
+    // Hämtar elementet med ID btn
     var btn = document.getElementById("btn");
 
+
+    // Funktion som skapar ett meddelande och skickar till rätt adress
     btn.onclick = function () {
+
+        // Hämtar meddelandet som ska skickas
         var message = document.getElementById("message").value;
+
+        // Hämtar iframen dit meddelandet ska skickas
         var iframe = document.getElementsByTagName('iframe')[0].contentWindow;
 
+        // Skickar meddelandet
         iframe.postMessage(message, "http://127.0.0.1:5500/Uppgift%203.4.1/html/iframe.html");
     }
 }   
