c0ad13b268e1418e4976d973529cc121a08ed20e
mJarsater <Martin.Jarsater@hotmail.com>
2020-12-04 11:27:01 +0100

    3.4.5

--- Uppgift 3.4.5/index.js
+++ Uppgift 3.4.5/index.js
@@ -1,6 +1,8 @@
 
+// Hämtar en ny EventSource
 var source = new EventSource("https://people.dsv.su.se/~pierre/i/05_ass/ip3/3/3.4/3.4.5/example.php");
 
+// Funktion som skriver ut datan från sourcen varje gång den skickar ett meddelande
 source.onmessage = function (event) {
     document.getElementById("result").innerHTML += event.data + "<br>";
 };
