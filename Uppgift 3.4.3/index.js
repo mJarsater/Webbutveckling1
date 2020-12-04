a333dd28119cf559d6e7136a7fc7aa559909a1d6
mJarsater <Martin.Jarsater@hotmail.com>
2020-12-04 10:50:26 +0100

    3.4.3

--- Uppgift 3.4.3/index.js
+++ Uppgift 3.4.3/index.js
@@ -1,18 +1,32 @@
 window.onload = function () {
+    // Hämtar elementet med ID "btn"
     var btn = document.getElementById("btn");
 
+    // Funktion som skickar ett httprequest
     btn.onclick = function () {
+        // Hämtar innehållet i elemnten med respektive ID
         var number1 = document.getElementById("number1").value;
-        var number2 = document.getElementById("number2").value
+        var number2 = document.getElementById("number2").value;
+
+        // Variabel för domänen som ska ta emot vårt httprequest
         var domain = `https://people.dsv.su.se/~pierre/i/05_ass/ip3/3/3.4/3.4.3/example.php?number1=${number1}&number2=${number2}`;
+        // Skapar ett nytt Httprequest object
         var xhttp = new XMLHttpRequest();
+
+        // Initierar httprequestet
+        xhttp.open("GET", domain, true);
+
+        // Skickar requestet
+        xhttp.send();
+
+        // Funktion som körs när readystate har ändrats
         xhttp.onreadystatechange = function () {
+            // Dubbelkollar så att requestet är klart
             if (xhttp.readyState == XMLHttpRequest.DONE) {
-                console.log(this.responseText);
+                /*  Skriver ut det som returneras från requestet(this.responseText) till sidan*/
                 document.getElementById("responseText").innerHTML = this.responseText;
             }
         };
-        xhttp.open("GET", domain, true);
-        xhttp.send();
+
     }
 }
