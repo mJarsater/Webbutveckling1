008f89625ebf04d303e5a1117e72771c6380047f
mJarsater <Martin.Jarsater@hotmail.com>
2020-12-04 11:39:24 +0100

    3.5.1

--- Uppgift 3.5.1/index.js
+++ Uppgift 3.5.1/index.js
@@ -1,21 +1,33 @@
+
+// Hämtar alla boxar till en array
 var boxes = Array.from(document.getElementsByClassName("box"));
 
-function selectBox(id) {
-    boxes.forEach(b => {
-        b.classList.toggle('selected', b.id == id);
-    });
-}
-
+// Loopar över boxarna och lägger till en eventlistner på varje
 boxes.forEach(b => {
     var id = b.id;
     b.addEventListener('click', e => {
+        // Eventlistner som lägger till ett state till history
         history.pushState({ id }, 'Titel', `./selected=${id}`)
+        // Kallar på selectBox med rätt ID
         selectBox(id);
     });
 });
 
+// Funktion som togglar klassen för rätt box
+function selectBox(id) {
+    boxes.forEach(b => {
+        // Toggla till selected om id matchar box.id
+        b.classList.toggle('selected', b.id == id);
+    });
+}
+
+/*  Denna används för att visa vilken som är iklickad om användare
+    hoppar framåt och bakåt på sidan    */
+// Lägger en eventlistner på window
 window.addEventListener('popstate', e => {
+    // Kollar om det finns något tillagt state för window
     if (e.state !== null)
+        // Om det finns ett state så kalla på selectBox för rätt ID
         selectBox(e.state.id);
     else
         selectBox(null);
